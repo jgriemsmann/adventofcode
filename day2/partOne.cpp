47745e27a9fdc52de985bfd1f034836aeb43f054
jgriemsmann <2715270+jgriemsmann@users.noreply.github.com>
2020-12-02 21:45:05 +0100

    feat: complete day2 exercises

---
+++ day2/partOne.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+  std::ifstream inputFile("input_em.txt");
+  std::string line;
+
+  int amountValidPasswords;
+
+  if (inputFile.is_open()) {
+    while(getline(inputFile, line)) {
+      std::string policy = line.substr(0, line.find(":"));
+      char policyLetter = policy.substr(policy.size()-1)[0];
+      std::string password = line.substr(policy.size()+2, line.size());
+      std::string lowestAsString = policy.substr(0, policy.find("-"));
+      int lowest = std::stoi(policy.substr(0, policy.find("-")));
+      std::string highestAsString = policy.substr(lowestAsString.size()+1, policy.size());
+      int highest = std::stoi(highestAsString);
+
+      int policyEvaluationCounter = 0;
+      for (auto it = password.begin(); it != password.end(); ++it) {
+        if (*it == policyLetter) {
+          policyEvaluationCounter++;
+        }
+      }
+      if (policyEvaluationCounter >= lowest && policyEvaluationCounter <= highest) {
+        amountValidPasswords++;
+      }
+    }
+  }
+  std::cout << "Amount of valid passwords: " << amountValidPasswords << std::endl;
+}
